0c0272526af7808a1ddf260d082794fdcd35d852
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-14 17:38:22 +0100

    Création du module agents

--- forestai/agents/__init__.py
+++ forestai/agents/__init__.py
@@ -1,5 +1,4 @@
-# forestai/agents/__init__.py
-
-from .base_agent import BaseAgent
-
-__all__ = ['BaseAgent']
+"""
+Module agents du système ForestAI.
+Contient les différents agents spécialisés (GeoAgent, ReglementationAgent, SubsidyAgent, etc.).
+"""
