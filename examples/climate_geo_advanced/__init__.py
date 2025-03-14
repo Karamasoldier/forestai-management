50a162e3d07cfe408e6e2df8191497fd8ef5dda8
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-14 08:16:42 +0100

    Création du fichier __init__.py pour le module climate_geo_advanced

---
+++ examples/climate_geo_advanced/__init__.py
@@ -0,0 +1,18 @@
+"""
+Package pour l'analyse forestière avancée combinant analyses géospatiales et climatiques.
+
+Ce package est une version modulaire du script climate_geo_advanced_example.py,
+décomposé en plusieurs modules spécialisés pour une meilleure maintenabilité et réutilisabilité.
+
+Modules :
+- data_preparation : Préparation et gestion des données (géométries, parcelles)
+- geo_analysis : Analyse géographique des parcelles
+- climate_analysis : Analyse climatique des parcelles
+- recommendation_engine : Filtrage et recommandation d'espèces
+- visualization : Création de visualisations et graphiques
+- report_generator : Génération de rapports
+- main : Programme principal orchestrant l'ensemble
+"""
+
+# Version du package
+__version__ = "1.0.0"
