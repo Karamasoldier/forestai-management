364eb86433cd1d8e68ea5c1186fbda6eaf115a6e
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-14 17:41:46 +0100

    Initialisation du module des services de domaine

--- forestai/core/domain/services/__init__.py
+++ forestai/core/domain/services/__init__.py
@@ -1,11 +1,4 @@
 """
-Module d'initialisation des services de domaine.
-
-Facilite l'importation des services de domaine.
+Module des services de domaine ForestAI.
+Contient les services métier utilisés par les agents.
 """
-
-# Import des services principaux pour les rendre accessibles directement
-from forestai.core.domain.services.climate_analyzer import ClimateAnalyzer
-from forestai.core.domain.services.climate_data_loader import ClimateDataLoader
-from forestai.core.domain.services.climate_zone_analyzer import ClimateZoneAnalyzer
-from forestai.core.domain.services.species_recommender import SpeciesRecommender
