de1a96cce0ef1b5f9179ca8a621f8d1fc0513742
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-15 18:27:06 +0100

    Ajout du module d'initialisation pour les validateurs de documents

---
+++ forestai/agents/document_agent/validators/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Module de validation pour le DocumentAgent.
+
+Ce package contient les classes et utilitaires pour valider les documents
+générés par le DocumentAgent selon différents critères.
+"""
+
+from forestai.agents.document_agent.validators.document_validator import DocumentValidator
+
+__all__ = ['DocumentValidator']
