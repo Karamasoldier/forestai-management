d2c893c31da913df2d1c97b97c7a6c868574bc2a
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-15 18:23:39 +0100

    Ajout du module d'initialisation pour les templates

---
+++ forestai/agents/document_agent/templates/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Module de gestion des templates pour le DocumentAgent.
+
+Ce package contient les classes et utilitaires pour charger, valider et
+rendre les templates de documents utilisés par le DocumentAgent.
+"""
+
+from forestai.agents.document_agent.templates.template_manager import TemplateManager
+
+__all__ = ['TemplateManager']
