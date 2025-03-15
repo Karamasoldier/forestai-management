64948d3150d719705af846b1c28a7bdd9522dfd6
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-15 13:05:18 +0100

    Mise à jour des imports pour exposer ReportManager

--- forestai/agents/diagnostic_agent/report_generator/__init__.py
+++ forestai/agents/diagnostic_agent/report_generator/__init__.py
@@ -1,3 +1,4 @@
 from forestai.agents.diagnostic_agent.report_generator.report_generator import ReportGenerator, ReportFormat
+from forestai.agents.diagnostic_agent.report_generator.report_manager import ReportManager, ReportType
 
-__all__ = ['ReportGenerator', 'ReportFormat']
+__all__ = ['ReportGenerator', 'ReportFormat', 'ReportManager', 'ReportType']
