6cb35025838876a7571130320a079f1e6579a8a3
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-15 18:17:37 +0100

    Ajout du module DocumentAgent

---
+++ forestai/agents/document_agent/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Module principal du DocumentAgent pour ForestAI.
+
+Ce module contient l'agent responsable de la génération automatique de documents
+administratifs liés à la gestion forestière, tels que les contrats, cahiers des charges,
+et autres documents officiels.
+"""
+
+from forestai.agents.document_agent.document_agent import DocumentAgent
+
+__all__ = ['DocumentAgent']
