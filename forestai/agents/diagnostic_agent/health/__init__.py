e4c198584fe2d3608bd2bd3b8c686d6ea9c77018
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-15 13:24:54 +0100

    Ajout du fichier d'initialisation du package health

---
+++ forestai/agents/diagnostic_agent/health/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Package d'analyse sanitaire forestière pour le DiagnosticAgent.
+"""
+
+from forestai.agents.diagnostic_agent.health.health_analyzer import HealthAnalyzer
+from forestai.agents.diagnostic_agent.health.health_issue import HealthIssue
+
+__all__ = [
+    'HealthAnalyzer',
+    'HealthIssue'
+]
