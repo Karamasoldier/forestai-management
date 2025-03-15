75719150d1fc28f33b2e24d25bde0474dd3ee11a
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-15 18:24:32 +0100

    Ajout du module d'initialisation pour le stockage de documents

---
+++ forestai/agents/document_agent/storage/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Module de stockage pour le DocumentAgent.
+
+Ce package contient les classes et utilitaires pour stocker, récupérer et
+gérer les documents générés par le DocumentAgent.
+"""
+
+from forestai.agents.document_agent.storage.document_storage import DocumentStorage
+
+__all__ = ['DocumentStorage']
