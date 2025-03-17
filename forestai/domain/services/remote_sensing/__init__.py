e1a97533a35d71f5c76677d2ff7753c7fbf9016f
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-17 17:06:17 +0100

    Ajout du module d'intégration de télédétection

---
+++ forestai/domain/services/remote_sensing/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Module d'intégration des données de télédétection pour ForestAI.
+
+Ce module fournit des fonctionnalités pour l'analyse et l'intégration
+des données de télédétection (satellite, LIDAR) pour améliorer
+les recommandations forestières et l'analyse des parcelles.
+"""
+
+# Les imports seront activés au fur et à mesure de l'implémentation
+# from forestai.domain.services.remote_sensing.remote_sensing_analyzer import RemoteSensingAnalyzer
+# from forestai.domain.services.remote_sensing.satellite_data_processor import SatelliteDataProcessor
+# from forestai.domain.services.remote_sensing.lidar_data_processor import LidarDataProcessor
+# from forestai.domain.services.remote_sensing.vegetation_index_calculator import VegetationIndexCalculator
+# from forestai.domain.services.remote_sensing.models import RemoteSensingSource, ImageryType, LidarDataType
+
+__all__ = [
+    # 'RemoteSensingAnalyzer',
+    # 'SatelliteDataProcessor',
+    # 'LidarDataProcessor',
+    # 'VegetationIndexCalculator',
+    # 'RemoteSensingSource',
+    # 'ImageryType',
+    # 'LidarDataType'
+]
