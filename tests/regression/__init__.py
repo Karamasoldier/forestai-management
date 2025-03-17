2bcc520137ed31c40189e9c9966387d398c757ca
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-17 08:53:49 +0100

    Ajout du dossier de tests de régression

---
+++ tests/regression/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Tests de régression pour ForestAI.
+
+Ce package contient les tests de régression qui vérifient que les nouvelles 
+modifications n'introduisent pas de régressions dans les fonctionnalités existantes.
+"""
