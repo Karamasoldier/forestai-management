8092f9eff63c48b3477beac2b43ab74455b10ce4
Karamasoldier <loic.ledoux04@gmail.com>
2025-03-17 20:31:34 +0100

    Création du module d'intégration de télédétection

--- forestai/domain/services/remote_sensing/__init__.py
+++ forestai/domain/services/remote_sensing/__init__.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Module d'intégration des données de télédétection pour ForestAI.
+Module d'intégration de télédétection pour ForestAI.
 
-Ce module fournit des fonctionnalités pour l'analyse et l'intégration
-des données de télédétection (satellite, LIDAR) pour améliorer
-les recommandations forestières et l'analyse des parcelles.
+Ce module fournit des services permettant d'acquérir, traiter et analyser
+des données de télédétection (satellites, LIDAR, drones) dans le cadre de la
+gestion forestière.
 """
 
-# Les imports seront activés au fur et à mesure de l'implémentation
-# from forestai.domain.services.remote_sensing.remote_sensing_analyzer import RemoteSensingAnalyzer
-# from forestai.domain.services.remote_sensing.satellite_data_processor import SatelliteDataProcessor
-# from forestai.domain.services.remote_sensing.lidar_data_processor import LidarDataProcessor
-# from forestai.domain.services.remote_sensing.vegetation_index_calculator import VegetationIndexCalculator
-# from forestai.domain.services.remote_sensing.models import RemoteSensingSource, ImageryType, LidarDataType
+from forestai.domain.services.remote_sensing.remote_data_connector import RemoteDataConnector
+from forestai.domain.services.remote_sensing.satellite_data_provider import SatelliteDataProvider
+from forestai.domain.services.remote_sensing.lidar_data_provider import LidarDataProvider
+from forestai.domain.services.remote_sensing.data_processor import RemoteSensingDataProcessor
+from forestai.domain.services.remote_sensing.forest_metrics_calculator import ForestMetricsCalculator
+from forestai.domain.services.remote_sensing.models import (
+    RemoteSensingData,
+    SatelliteImageMetadata,
+    LidarPointCloudMetadata,
+    ForestMetrics,
+    VegetationIndex,
+    RemoteSensingSource
+)
 
 __all__ = [
-    # 'RemoteSensingAnalyzer',
-    # 'SatelliteDataProcessor',
-    # 'LidarDataProcessor',
-    # 'VegetationIndexCalculator',
-    # 'RemoteSensingSource',
-    # 'ImageryType',
-    # 'LidarDataType'
+    'RemoteDataConnector',
+    'SatelliteDataProvider',
+    'LidarDataProvider',
+    'RemoteSensingDataProcessor',
+    'ForestMetricsCalculator',
+    'RemoteSensingData',
+    'SatelliteImageMetadata',
+    'LidarPointCloudMetadata',
+    'ForestMetrics',
+    'VegetationIndex',
+    'RemoteSensingSource'
 ]
